e2f2bd8e5e8a0087b3fe9c7b59469f1f61804ff1
vinicius-vasconcelos <viniciussouzav@gmail.com>
2023-05-23 15:05:36 -0300

    Atualizando app.js, novos SQL e removendo PUTs

--- src/app.js
+++ src/app.js
@@ -7,8 +7,15 @@ const app = express();
 app.use(express.json());
 
 const WAITING_DRIVER = 1;
-const TRAVEL_STARTED = 2;
-const TRAVEL_FINISHED = 3;
+const DRIVER_ON_THE_WAY = 2;
+const TRAVEL_IN_PROGRESS = 3;
+const TRAVEL_FINISHED = 4;
+const statusArr = [
+  WAITING_DRIVER,
+  DRIVER_ON_THE_WAY,
+  TRAVEL_IN_PROGRESS,
+  TRAVEL_FINISHED,
+];
 
 const passengerExists = async (passengerId) => {
   const [[passenger]] = await connection.execute(
@@ -53,37 +60,46 @@ app.post('/passengers/:passengerId/request/travel', async (req, res) => {
 
 app.get('/drivers/open/travels', async (_req, res) => {
   const [result] = await connection.execute(
-    'SELECT * FROM travels WHERE travel_status_id = ?',
+    `SELECT TR.id, TR.passenger_id, TR.driver_id, TR.travel_status_id,
+    TR.travel_status_id, TR.starting_address, TR.ending_address, TR.request_date,
+      TS.status
+    FROM travels AS TR
+    INNER JOIN travel_status AS TS ON TR.travel_status_id = TS.id
+    WHERE TR.id = ?`
     [WAITING_DRIVER],
   );
   res.status(200).json(result);
 });
 
-app.put('/drivers/:driverId/travels/:travelId/assign', async (req, res) => {
-  const { travelId, driverId } = req.params;
-  await connection.execute(
-    'UPDATE travels SET driver_id = ? WHERE id = ?',
-    [driverId, travelId],
-  );
-  await connection.execute(
-    'UPDATE travels SET travel_status_id = ? WHERE id = ? AND driver_id = ?',
-    [TRAVEL_STARTED, travelId, driverId],
-  );
-  const [[result]] = await connection.execute(
-    'SELECT * FROM travels WHERE id = ?',
+// aula ao vivo
+app.put('/drivers/:driverId/travels/:travelId', async (req, res) => {
+  const { driverId, travelId } = req.params;
+  const [[travel_status_id]] = await connection.execute(
+    `SELECT TR.id, TR.passenger_id, TR.driver_id, TR.travel_status_id,
+    TR.travel_status_id, TR.starting_address, TR.ending_address, TR.request_date,
+      TS.status
+    FROM travels AS TR
+    INNER JOIN travel_status AS TS ON TR.travel_status_id = TS.id
+    WHERE TR.id = ?`
     [travelId],
   );
-  res.status(200).json(result);
-});
 
-app.put('/drivers/:driverId/travels/:travelId/end', async (req, res) => {
-  const { travelId, driverId } = req.params;
+  const nextTravelStatusId = travel_status_id + 1;
+  // REMOVER, isso será utilizado no dia de service
+  // const isValid = statusArr.includes(Number(nextTravelStatusId));
+  // if (!isValid) return res.status(409).end();
+
   await connection.execute(
     'UPDATE travels SET travel_status_id = ? WHERE id = ? AND driver_id = ?',
-    [TRAVEL_FINISHED, travelId, driverId],
+    [nextTravelStatusId, travelId, driverId],
   );
   const [[result]] = await connection.execute(
-    'SELECT * FROM travels WHERE id = ?',
+    `SELECT TR.id, TR.passenger_id, TR.driver_id, TR.travel_status_id,
+    TR.travel_status_id, TR.starting_address, TR.ending_address, TR.request_date,
+      TS.status
+    FROM travels AS TR
+    INNER JOIN travel_status AS TS ON TR.travel_status_id = TS.id
+    WHERE TR.id = ?`,
     [travelId],
   );
   res.status(200).json(result);
